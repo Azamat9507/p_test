1d7b24779b804d30a54155ed5e0de71b442d86c6
Azamat9507 <solijonov.azamat9507@gmail.com>
2023-02-06 04:31:32 +0900

    dotenv & project configuration

---
+++ calculate.js
@@ -0,0 +1,9 @@
+const cal = module.exports;
+
+cal.kopaytirish = (a, b) => {
+    return a * b;
+};
+
+cal.qosh = (a, b) => {
+    return a + b;
+}
---
+++ train.js
@@ -0,0 +1,59 @@
+console.log("ishga tushdi");
+const plus = require("./calculate.js");
+const moment = require("moment");
+const fs = require("fs");
+// console.log("Foydali maslahatlar");
+
+// const list = [
+//     "yaxshi talaba boling",
+//     "togri boshliq tanlang kopro hato qiling",
+//     "oziz uchun ishlang",
+//     "yoshlarga investitsiya qiling",
+//     "endi dam oling",
+// ];
+
+// function givemeAdvise(a, callback) {
+//     if (typeof a !== "number") callback("insert a number", null);
+//     else if (a < 20) callback(null, list)
+// }
+
+// class Person {
+//     constructor(yosh, ism, millat) {
+//         this.age = yosh,
+//         this.name = ism,
+//         this.nation = millat
+//     }
+
+//     tonishtiring() {
+//         console.log('mening ismim', this.name);
+//     }
+
+//     salom_bering() {
+//         console.log('salom bering');
+//     }
+
+//     hayerlashing() {
+//         console.log('salomat boling');
+//     }
+// }
+
+// const person_1 = new Person(28, 'hamid', 'uzbek')
+
+
+// const result = plus.kopaytirish(4, 8);
+// console.log(result);
+
+// console.log("-------------");
+
+
+// setTimeout(() => {
+//     const time = moment(). format("YYYY-MM-DD HH:mm:ss");
+//     const str =`hisob: ${result} ${time}`;
+//     fs.writeFileSync("./result.txt", str)
+// }, 10000);
+
+const arr = [0, 1, 2];
+console.log(arr.length);
+arr [10] = 10;
+
+console.log(arr.length);
